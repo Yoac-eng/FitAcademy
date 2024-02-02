4118ffbe76387dc885dc752c14e0220507691181
Yoac <cayocutrim1991@gmail.com>
2024-02-01 21:04:11 -0300

    first commit

---
+++ src/main/java/com/fitAcademy/fitAcademy/FitAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.fitAcademy.fitAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitAcademyApplication.class, args);
+	}
+
+}
